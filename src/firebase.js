43980392279489042698eecc416f29a0261f6c45
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-21 20:19:25 +0500

    Header Component Completed

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8L1YRhodWuIpo3zg-wuG4OaGaLlx_VHc",
+    authDomain: "facebook-with-reactjs.firebaseapp.com",
+    databaseURL: "https://facebook-with-reactjs.firebaseio.com",
+    projectId: "facebook-with-reactjs",
+    storageBucket: "facebook-with-reactjs.appspot.com",
+    messagingSenderId: "709260539642",
+    appId: "1:709260539642:web:7a76531da94777619a7b43",
+    measurementId: "G-ZH64EC49BM"
+  };
