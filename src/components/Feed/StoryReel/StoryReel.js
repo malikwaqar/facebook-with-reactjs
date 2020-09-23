2cde9b088abbac176747aeded9589f75910ead30
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-23 16:20:04 +0500

    StoryReels Component Completed

---
+++ src/components/Feed/StoryReel/StoryReel.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './StoryReel.css';
+import Story from './Story/Story';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story image="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
+            profileSrc="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            title="Malik Waqar" />
+
+            <Story image="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
+            profileSrc="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            title="Malik Waqar" />
+
+            <Story image="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
+            profileSrc="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            title="Malik Waqar" />
+
+            <Story image="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
+            profileSrc="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            title="Malik Waqar" />
+
+            <Story image="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"
+            profileSrc="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            title="Malik Waqar" />
+        </div>
+    )
+}
+
+export default StoryReel;
