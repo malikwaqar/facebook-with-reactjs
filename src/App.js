22c2a600dfc497bd5105c218293103da03c2132d
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-25 10:38:57 +0500

    Widgets Component Complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
+import Widgets from './components/Widgets/Widgets';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Widgets />
       </div>
     </div>
   );
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel/StoryReel';
 import AddPost from '../AddPost/AddPost';
+import Post from '../Post/Post';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
             <AddPost />
+            <Post
+            profilePic="https://avatars1.githubusercontent.com/u/49842187?s=460&u=f40259e1e37e9346d757848196904a23dbd664da&v=4"
+            message="Let's Dance..."
+            timestamp="12:40AM"
+            username="Malik Waqar"
+            image="https://media0.giphy.com/media/1BfQlyaAp4eavGzaYE/giphy.gif"
+            />
         </div>
     )
 }
